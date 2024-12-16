e810de4717324b2b969ca6ccb32eb8af2876630b
Syed Murtaza Salman <u2023694@giki.edu.pk>
2024-12-16 15:41:39 +0500

    Pushing the structures for the train and the BST

---
+++ Project.cpp
@@ -0,0 +1,55 @@
+#include <iostream>  // For input/output
+#include <string>    // For strings
+#include <sstream>   // For stringstream (if used)
+#include <fstream>   // For file operations
+#include <ctime>     // For time functions
+#include <vector>    // For vectors in trainRoutes
+#include <map>       // For maps in trainRoutes
+#include <algorithm> // For sort function (if needed)
+#include <cstdlib>   // For system("cls"), rand(), and exit()
+#include <Windows.h> // For Sleep()
+
+using namespace std;
+int main();
+
+map<string, vector<string>> trainRoutes;
+
+struct node
+{
+    int ticketID;
+    string Name, Age, NIC, Contact, BookedClass, BookedTName, BookedTID, BookedDest, BookedSource, BookedDate, DeptTime, ArrTime, BookedSeatNum;
+    float Price;
+    node *next;
+};
+
+struct trains
+{
+    string TName, TID, TDest, TSource, TDate, arriveTime, departTime, TClasses;
+    int TSeats;
+};
+
+struct TrainBST
+{
+    trains data;
+    TrainBST *left;
+    TrainBST *right;
+};
+TrainBST *trainRoot = NULL;
+
+struct SeatRecord
+{
+    string RowA;
+    string RowB;
+    string RowC;
+};
+
+node *head = NULL;
+node *tail = NULL;
+
+string Name, Age, NIC, Contact, Dest, Source, SeatNum, Class, TID, Date, ATime, DTime, TName;
+bool trainCheck = false;
+float Price;
+string BookedID;
+int currentID;
+trains availTrains[10];
+int noOfTrains = 5;
